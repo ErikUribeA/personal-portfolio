7da59fed84a35d0ad620e53f687bac368f519b33
Erik <erickjosueuribe@gmail.com>
2024-10-18 12:20:31 -0500

    improving the page

--- src/app/(main)/page.tsx
+++ src/app/(main)/page.tsx
@@ -7,7 +7,7 @@ export default function Home() {
   const translate = useTranslations("home")
 
   return (
-    <section className="min-h-screen flex items-center align-middle justify-center ">
+    <section className="min-h-[calc(100vh-200px)] lg:min-h-[calc(100vh-100px)] flex items-center  justify-center ">
       <div className="text-center">
         <h1 className="text-6xl font-bold mb-4">
           <AnimatedLetters
@@ -18,7 +18,7 @@ export default function Home() {
         <p className="text-2xl mb-8">{translate("subTitle")}</p>
         <Link
           href="/about"
-          className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hover:to-pink-600 text-white font-bold py-2 px-6 rounded-full transition-all duration-300 transform hover:scale-105"
+          className="bg-gradient-to-r from-purple-500 to-pink-500 hover:from-purple-600 hoverlg:to-pink-600 text-white font-bold py-2 px-6 rounded-full transition-all duration-300 transform hover:scale-105"
         >
           {translate("button")}
         </Link>
